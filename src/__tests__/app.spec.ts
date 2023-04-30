cd4faa8605e5bc6022f2d14e13571b2b68f41248
Lais Frigerio <lais.frigerio@nubank.com.br>
2023-04-30 11:11:48 -0300

    Adding eslint configuration

--- src/__tests__/app.spec.ts
+++ src/__tests__/app.spec.ts
@@ -2,9 +2,9 @@ import request from "supertest";
 import app from "../app";
 
 it("Should return a `Hello World` message", async () => {
-    await request(app).get("/")
+  await request(app).get("/")
     .then((response) => {
-        expect(response.status).toBe(200);
-        expect(response.body.message).toBe("Hello World");
+      expect(response.status).toBe(200);
+      expect(response.body.message).toBe("Hello World");
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (_, response) => {
-    return response.status(200).json({ message: "Hello World" });
-})
+  return response.status(200).json({ message: "Hello World" });
+});
 
 export default app;
