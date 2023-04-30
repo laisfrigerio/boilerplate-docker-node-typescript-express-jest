34a2d6e0c2b250846f3bdb62bdc3a49d9eca0a52
Lais Frigerio <lais.frigerio@nubank.com.br>
2023-04-30 10:51:19 -0300

    adding the base server configuration

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_, response) => {
+    return response.status(200).json({ message: "Hello World" });
+})
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import app from "./app";
+
+const PORT = 3333;
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
